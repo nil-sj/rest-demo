942ed74a1f99e97b1bbe7168127b6cd8a634377a
Niladri Sekhar Jyoti <nil@Niladris-MacBook-Pro.local>
2024-07-07 23:50:23 -0400

    resolved CORS error

--- src/main/java/com/tc/rest_demo/RestDemoApplication.java
+++ src/main/java/com/tc/rest_demo/RestDemoApplication.java
@@ -2,12 +2,30 @@ package com.tc.rest_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestDemoApplication.class, args);
+	}
+
+	// To prevent CORS issue with local development
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/cloudvendor/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 }
